590e5af5541a259f00508935672185310a8283fb
jPruim <jasonpruim@gmail.com>
2024-08-22 02:13:23 -0400

    remove conversion code

--- recipee.pruim/src/app/pages/table-of-contents/table-of-contents.page.ts
+++ recipee.pruim/src/app/pages/table-of-contents/table-of-contents.page.ts
@@ -19,71 +19,13 @@ export class TableOfContentsPage implements OnInit {
     return cleaned.replace(/^[\t ]*"[^:\n\r]+(?<!\\)":/gm, function (match) {
         return match.replace(/"/g, "");
     });
-
-    // originally in ngOnInit, this was the code that transferred the data between the formats
-    // let recipeArray2: Recipe2[] = [];
-    // let recipe2 : Recipe2 = {
-    //   title: "",
-    // }
-    // let pageSegment: PageSegment ={};
-    // this.recipeArray.forEach(el => {
-    //   console.log(JSON.stringify(el.title));
-    //   recipe2.title = el?.title ?? "";
-    //   el?.originalBookID && (recipe2.originalBookID = el?.originalBookID);
-    //   el?.recipeNumber && (recipe2.recipeNumber = el?.recipeNumber);
-    //   el?.preparationTime && (recipe2.preparationTime = el?.preparationTime);
-    //   el?.categoryID && (recipe2.categoryID = el?.categoryID);
-    //   el?.lastEdit && (recipe2.lastEdit = el?.lastEdit);
-    //   el?.intro && (pageSegment.intro = el?.intro);
-    //   el?.imageLink && (pageSegment.imageLink = el?.imageLink);
-    //   el?.steps && (pageSegment.steps = el?.steps);
-    //   el?.description && (pageSegment.description = el?.description);
-    //   el?.reviews && (pageSegment.reviews = el?.reviews);
-    //   el?.outro && (pageSegment.outro = el?.outro);
-    //   el?.links && (pageSegment.links = el?.links);
-    //   recipe2.segments = [pageSegment];
-    //   recipeArray2.push(recipe2);
-    // });
-    // console.log("JSON Object");
-    // console.log(this.cleanIt(recipeArray2));
   }
+
   ngOnInit() {
     let oldData = RecipeDataOld;
     this.recipeArray = RecipeData;
     this.shortenDescriptions();
     let newData: any = [];
-    // oldData.forEach ( (el) => {
-    //   newData.push(this.cleanIt(el));
-    // })
-    // console.log(this.cleanIt(oldData));
-
-
-    let recipeArray2: Recipe[] = [];
-    
-    oldData.forEach(el => {
-      console.log(el.title);
-      let recipe2 : Recipe = {
-        title: "",
-      }
-      let pageSegment: PageSegment ={};
-      recipe2.title = el?.title ?? "";
-      el?.originalBookID && (recipe2.originalBookID = el?.originalBookID);
-      el?.recipeNumber && (recipe2.recipeNumber = el?.recipeNumber);
-      el?.preparationTime && (recipe2.preparationTime = el?.preparationTime);
-      el?.categoryID && (recipe2.categoryID = el?.categoryID);
-      el?.lastEdit && (recipe2.lastEdit = el?.lastEdit);
-      el?.intro && (pageSegment.intro = el?.intro);
-      el?.imageLink && (pageSegment.imageLink = el?.imageLink);
-      el?.steps && (pageSegment.steps = el?.steps);
-      el?.description && (pageSegment.description = el?.description);
-      el?.reviews && (pageSegment.reviews = el?.reviews);
-      el?.outro && (pageSegment.outro = el?.outro);
-      el?.links && (pageSegment.links = el?.links);
-      recipe2.segments = [{...pageSegment}];
-      recipeArray2.push(JSON.parse(JSON.stringify(({...recipe2}))));
-    });
-    console.log("JSON Object");
-    console.log(recipeArray2);
   }
 
   shortenDescriptions(){
